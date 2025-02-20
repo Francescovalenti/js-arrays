0285533502c66728a809a5e0df27879ddac39d8a
francescovalenti <francescovalenti83@outlook.it>
2025-02-20 21:51:10 +0100

    feat:1

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -15,10 +15,13 @@ const fourthTeacher = null;
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop();
+console.log(lastTeacher)
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift();
+console.log(firstTeacher)
+
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
